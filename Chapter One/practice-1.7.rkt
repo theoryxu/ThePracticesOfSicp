b1af02e4a14a0b4fc238e9eaa74319b1c31592c2
theoryxu <xuxiaotheory@gmail.com>
2015-01-07 00:05:37 +0800

    task

--- Chapter One/practice-1.7.rkt
+++ Chapter One/practice-1.7.rkt
@@ -17,3 +17,15 @@
       (sqrt-iter (improve guess x) x)))
 
 (sqrt-iter 1 2)
+(sqrt-iter 1 0.000016)
+
+;改进牛顿法求平方根
+(define (new-good-enough? guess previous-guess)
+  (< (abs (/ (- previous-guess guess) guess)) 0.0001))
+
+(define (new-sqrt-iter guess previous-guess x)
+  (if (new-good-enough? guess previous-guess)
+      guess
+      (new-sqrt-iter (improve guess x) guess x)))
+
+(new-sqrt-iter 1 0 0.000016)
